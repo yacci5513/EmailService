858e89d4f57554e951e7ce0ed2313a82a128cd75
yacci5513 <ckstjddh1@naver.com>
2024-03-27 16:27:17 +0900

    20240327 migrate 초기값 삭제

--- database/migrations/2024_03_27_064629_create_users_table.php
+++ database/migrations/2024_03_27_064629_create_users_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('u_id');
+            $table->string('u_email');
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('users');
     }
 };
